1072806c7ac50f608e9e107c439d9d5908a8ed81
Peter Goshulak <pgoshulak@gmail.com>
2018-04-11 16:35:36 +0000

    Complete counting function

--- count-letters.js
+++ count-letters.js
@@ -1,6 +1,14 @@
 var countLetters = (str) => {
-  var letters = {str:str}
+  var letters = {}
   // Count the letters
+  for (var char of str) {
+    // Check if char is counted already
+    if (letters[char] > 0) {
+      letters[char]++
+    } else {
+      letters[char] = 1
+    }
+  }
 
   // return object with letter:count pairs
   return letters
