8cc55f256f3d46cf2df5c421f0a884237ad2f617
Peter Goshulak <pgoshulak@gmail.com>
2018-04-11 16:24:03 +0000

    Initialize main function

---
+++ count-letters.js
@@ -0,0 +1,9 @@
+var countLetters = (str) => {
+  var letters = {str:str}
+  // Count the letters
+
+  // return object with letter:count pairs
+  return letters
+}
+
+console.log(countLetters(process.argv[2]))
